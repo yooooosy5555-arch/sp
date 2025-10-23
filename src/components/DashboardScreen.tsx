f4575f5f98324902ed2595ccabb3a3f8d9e3a2ef
yooooosy5555-arch <yooooosy5555@gmail.com>
2025-10-23 13:40:59 +0900

    Update DashboardScreen.tsx

--- src/components/DashboardScreen.tsx
+++ src/components/DashboardScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Bell, MessageSquare, PlusCircle } from 'lucide-react';
 
 export function DashboardScreen(): JSX.Element {
